0d54bf2747d764623b3ddc7adcb717908a08738b
momashahra22-debug <momashahra22@gmail.com>
2026-09-17 03:21:15 +0300

    Bump cache for simple YouTube player

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const VERSION='studymate-v21';
+const VERSION='studymate-v22';
 const APP_CACHE=`${VERSION}-app`;
 const DATA_CACHE=`${VERSION}-data`;
-const APP_SHELL=['./','./index.html','./js/css-loader.js?v=21','./js/app-loader.js?v=21','./js/config.js?v=21'];
+const APP_SHELL=['./','./index.html','./js/css-loader.js?v=22','./js/app-loader.js?v=22','./js/config.js?v=22','./js/youtube-simple.js?v=22'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(APP_SHELL)).catch(()=>null));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>!k.startsWith(VERSION)).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
